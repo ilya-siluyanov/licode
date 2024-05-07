aa438ee1bccf5ff2f8fa9608bf721efe0bd1baa9
Ilya Siluyanov <isiluyanov8@gmail.com>
2024-05-07 10:45:21 +0300

    [wip]

--- client/main.py
+++ client/main.py
@@ -1,19 +1,33 @@
 import time
 import sys
 from selenium.webdriver import Chrome, ChromeOptions, ChromeService
+from selenium.webdriver.common.by import By
 
 
 path = sys.argv[1]
+headless = int(sys.argv[2])
 
 options = ChromeOptions()
 options.add_argument("use-fake-device-for-media-stream")
 options.add_argument("use-fake-ui-for-media-stream")
+options.add_argument("ignore-certificate-errors")
+options.add_argument("no-sandbox")
+options.add_argument("disable-dev-shm-usage")
+if headless:
+    options.add_argument("--headless=new")
 
 service = ChromeService(executable_path='/Users/ilyasiluyanov/Downloads/yandexdriver')
 driver = Chrome(options=options, service=service)
 try:
     driver.get("https://ui.webrtc-thesis.ru")
     driver.implicitly_wait(5)
-    time.sleep(10000)
+    while True:
+        time.sleep(5)
+        elements = driver.find_elements(By.CLASS_NAME, "candidatetext")
+        for element in elements:
+            id_ = element.get_dom_attribute("id")
+            text = element.text
+            print(f"{id_}: {text}")
 finally:
+    print("close driver")
     driver.close()
